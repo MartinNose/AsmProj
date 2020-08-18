30fd1717486e8cb7a7bbf2eeda3a0adc254e98d0
MartinNose <liucmartin@qq.com>
2020-08-18 16:49:06 +0800

    little fix

--- proj2/src/game.s
+++ proj2/src/game.s
@@ -5,108 +5,157 @@ main:
     ori $s2, $s2, 4880
     addi $s4, $zero, 0x074f
     addi $t0, $zero, 0
-    main_loop:
-        sw $s4, 0($s2)
-        addi $s2, $s2, 9920
-        sw $s4, 0($s2)
-        addi $s2, $s2, -9920
-        addi $s2, $s2, 4
-        addi $t0, $t0, 1
-        slti $t3, $t0, 40
-        bne $t3, $zero, main_loop
-
-    lui $s2, 0x000C #VGACursor
-    ori $s2, $s2, 4876
-    addi $t0, $zero, 0
-    main_loop_2:
-        sw $s4, 0($s2)
-        addi $s2, $s2, 164
-        sw $s4, 0($s2)
-        addi $s2, $s2, -164
-        addi $s2, $s2, 320
-        addi $t0, $t0, 1
-        slti $t3, $t0, 32
-        bne $t3, $zero, main_loop_2
     jal update_Ob
+    j game
     # Border drawn
+drawBlock:
+    addi $sp, $sp, -64
+    sw $ra, 4($sp)
+    sw $s0, 8($sp)
+    sw $s1, 12($sp)
+    sw $s2, 16($sp)
+    sw $s3, 20($sp)
+    sw $s4, 24($sp)
+    sw $s5, 28($sp)
+    sw $s6, 32($sp)
+    sw $t0, 36($sp)
+    sw $t1, 40($sp)
+    sw $t2, 44($sp)
+    sw $t3, 48($sp)
+    sw $t4, 52($sp)
+    sw $t5, 56($sp)
+    sw $t6, 60($sp)
+    sw $t7, 64($sp)
+
+    addi $s0, $a0, 0 # col 0-40
+    addi $s1, $a1, 0 # row 0-30
+    addi $s2, $a2, 0 # RGB color
+    
+    addi $s3, $zero, 16 # block size
+    addi $s4, $zero, 640 # column
+    addi $s5, $zero, 480 # row
+
+    add $t0, $zero, $zero # VGA cursor
+    add $t1, $zero, $zero # vga_column count
+    add $t2, $zero ,$zero # vga_row count
+    add $t3, $zero, $zero # grid_column counter
+    add $t4, $zero, $zero # grid_row counter
+    lui $t5, 0xc000
+
+    move_cursor_col:
+    addi $t0, $t0, 1
+    addi $t1, $t1, 1
+    bne $t1, $s3, move_cursor_col
+    add $t1, $t1, $zero
+    add $t3, $t3, 1
+    bne $t3, s0, move_cursor_col
+    
+    move_cursor_row:
+    addi $t0, $t0, 640
+    addi $t2, $t2, 1
+    bne $t2, $s3, move_cursor_row
+    add $t2, $t2, 0
+    add $t4, $t4, 1
+    bne $t4, $s5, move_cursor_row
+
+    add $t1, $zero, $zero # vga_column count
+    add $t2, $zero ,$zero # vga_row count
+
+    addi $t6, $zero, 0xdf2
+    bne $s2, $t6, draw_start
+    lw $t7, 0($t0)
+    beq $t7, $t6, game_over
+    
+    draw_start:
+    sw $s2, 0($t0)
+    addi $t0, $t0, 1
+    addi $t1, $t1, 1
+    bne $t1, $s3, draw_start
+    add $t1, $t1, 0
+    addi $t0, $t0, -16
+    addi $t0, $t0, 640
+    addi $t2, $t2, 1
+    bne $t2, $t2, draw_start
+
+    lw $ra, 4($sp)
+    lw $s0, 8($sp)
+    lw $s1, 12($sp)
+    lw $s2, 16($sp)
+    lw $s3, 20($sp)
+    lw $s4, 24($sp)
+    lw $s5, 28($sp)
+    lw $s6, 32($sp)
+    lw $t0, 36($sp)
+    lw $t1, 40($sp)
+    lw $t2, 44($sp)
+    lw $t3, 48($sp)
+    lw $t4, 52($sp)
+    lw $t5, 56($sp)
+    lw $t6, 60($sp)
+    lw $t7, 64($sp)
+    addi $sp, $sp, 64
+    jr $ra
 
 game:
     lui $s1, 0
     ori $s1, $zero, 0x042c # time count
     lw $s0, 0($s1)
-    lui $s2, 0x0009
+    lui $s2, 0x0009 # loop gap
     bne $s0, $s2, no_update_Ob
         jal update_Ob
         ori $s0, $zero, 0
     no_update_Ob:
     addi $s0, $s0, 1
-    sw $s0, 0($s1)
-    #read PS and process 
+    sw $s0, 0($s1) # increment time count
+    #read PS and process
     lui $s0, 0xffff
     ori $s0, $s0, 0xd000 # PS2 Addr
     ori $s1, $zero, 0x0428 # duck position Addr
     lw $t7, 0($s0) # Read PS2
     lui $t6, 0x8000
-    ori $t6, $t6, 0x001D
+    ori $t6, $t6, 0x001D # if W
+    
     lw $t3, 0($s1) # Read Duck Position
+
     bne $t7, $t6, no_w
-        ori $t4, $zero, 1
+        addi $t4, $zero, 1
         beq $t3, $t4, top
             addi $t3, $t3, -1
         top:
         j draw
     no_w:
     lui $t6, 0x8000
-    ori $t6, $t6, 0x00F0
+    ori $t6, $t6, 0x00F0 # if 
     bne $t7, $t6, no_duan_ma
-        lw $t7, 0($s0)
+        lw $t7, 0(s0) # Process DuanMa
         ori $t7, $zero, 0
         j draw
+
     no_duan_ma:
     lui $t6, 0x8000
     ori $t6, $t6, 0x001B
-    bne $t7, $t6, no_s
-        ori $t2, $zero, 30
-        beq $t3, $t2, bottom
+    bne $t7, $t6, draw # If S
+        addi $t2, $zero, 30
+        beq $t3, $t2, draw
             addi $t3, $t3, 1
-        bottom:
-        j draw
 
-    no_s:
-        sll $zero, $zero, 0
     # draw duck and judge
     draw:
         lw $t0, 0($s1) # Read Positon
-        beq $t0, $t3, no_cover
-        ori $t1, $zero, 0
-        lui $s2, 0x000C
-        ori $s2, $s2, 4904 # Bird Cursor
-        draw_duck_loop:
-            addi $t1, $t1, 1
-            addi $s2, $s2, 320
-        bne $t1, $t0, draw_duck_loop
-        lw $t0, 0($s2)
-        ori $t1, $zero, 0x202f
-        beq $t0, $t1, game_over
-        ori $t1, $zero, 0x3000
-        sw $t1, 0($s2) # draw duck
+        beq $t0, $t3, no_cover # If Position is unaltered.
 
-        sw $t3, 0($s1)
+        addi $a0, $zero, 6 # 6th col
+        addi $a1, $t0, 0 # older row
+        addi $a2, $zero, 0xcc4 # draw blue
+        jal drawBlock
+
+        sw $t3, 0($s1) # save the duck position
+
     no_cover:
-        lw $t0, 0($s1) # Read Positon
-        ori $t1, $zero, 0
-        lui $s2, 0x000C
-        ori $s2, $s2, 4904 # Bird Cursor
-        draw_duck_loop1:
-            addi $t1, $t1, 1
-            addi $s2, $s2, 320
-        bne $t1, $t0, draw_duck_loop1
-        lw $t0, 0($s2)
-        ori $t1, $zero, 0x202f
-        beq $t0, $t1, game_over
-        ori $t1, $zero, 0x6600
-        sw $t1, 0($s2) # draw duck
-        
+        addi $a0, $zero, 6
+        addi $a1, $t3, $zero
+        addi $a2, $zero, 0xdf2 # draw yellow
+        jal drawBlock
 j game
 
 game_over:
@@ -160,50 +209,72 @@ draw_ob:
     sw $s2, 16($sp)
 
     ori $s0, $zero, 0x0400 # ob info
-    lui $s1, 0x000C
-    ori $s1, $s1, 5200 # vga cursor
 
-    ori $t0, $zero, 0
-    ori $t1, $zero, 10
+    #lui $s1, 0x000C
+    #ori $s1, $s1, 5200 # vga cursor
+
+    addi $t0, $zero, 0 # slots counter
+    addi $t1, $zero, 10 # Totally 10 slots
+    addi $t3, $zero, 0 # col counter
     draw_Ob_Outter_loop: # draw t0 th ob
-        addi $t0, $t0, 1
-        lw $s4, 0($s0) # load ob info to s4
-        ori $s2, $zero, 0x202f # green o
-        bne $s4, $zero, green
-            ori $s2, $zero, 0x3000 #black O
+        lw $s4, 0($s0) # load openning start point
+
+        ori $s2, $zero, 0x3b7 # green
+
+        bne $s4, $zero, green # If there exists an pipe
+            ori $s2, $zero, 0xcc4 # blue
             ori $s4, $zero, 5
+
         green:
-        ori $t2, $zero, 0
+        addi $t2, $zero, 0 # Set row counter
         draw_Ob_Inner_loop1: # draw upper ob
+            addi $a0, $t3, 0
+            addi $a1, $t2, 0
+            addi $a2, $s2, 0
+            jal drawBlock
+            addi $a0, $a0, 1
+            jal drawBlock
+            addi $a0, $a0, 1
+            jal drawBlock
+            addi $a0, $a0, 1
+            jal drawBlock
             addi $t2, $t2, 1
-            sw $s2, 0($s1)
-            sw $s2, 4($s1)
-            sw $s2, 8($s1)
-            sw $s2, 12($s1)
-            addi $s1, $s1, 320
         bne $t2, $s4, draw_Ob_Inner_loop1
+
         addi $s4, $s4, 5
-        ori $s5, $zero, 0x3000 # black o
+        ori $s5, $zero, 0xcc4 # blue
         draw_Ob_Inner_loop2: # draw upper ob
+            addi $a0, $t3, 0
+            addi $a1, $t2, 0
+            addi $a2, $s5, 0
+            jal drawBlock
+            addi $a0, $a0, 1
+            jal drawBlock
+            addi $a0, $a0, 1
+            jal drawBlock
+            addi $a0, $a0, 1
+            jal drawBlock
             addi $t2, $t2, 1
-            sw $s5, 0($s1)
-            sw $s5, 4($s1)
-            sw $s5, 8($s1)
-            sw $s5, 12($s1)
-            addi $s1, $s1, 320
         bne $t2, $s4, draw_Ob_Inner_loop2
+
         ori $s4, $zero, 30
         draw_Ob_Inner_loop3: # draw lower ob
+            addi $a0, $t3, 0
+            addi $a1, $t2, 0
+            addi $a2, $s5, 0
+            jal drawBlock
+            addi $a0, $a0, 1
+            jal drawBlock
+            addi $a0, $a0, 1
+            jal drawBlock
+            addi $a0, $a0, 1
+            jal drawBlock
             addi $t2, $t2, 1
-            sw $s2, 0($s1)
-            sw $s2, 4($s1)
-            sw $s2, 8($s1)
-            sw $s2, 12($s1)
-            addi $s1, $s1, 320
         bne $t2, $s4, draw_Ob_Inner_loop3
-        addi $s1, $s1, -9600
-        addi $s1, $s1, 16
-        addi $s0, $s0, 4
+
+        addi $s0, $s0, 4 # update ob address
+        addi $t0, $t0, 1 # update ob counter
+        addi $t3, $t3, 4 # update col counter
     bne $t0, $t1, draw_Ob_Outter_loop
 
     lw $ra, 4($sp)
